5dbbbd86385a89e0e386ea2573fc063a93d06f7e
Owen Murphy <owenmurphy@Owens-MacBook-Pro-2.local>
2022-07-06 15:30:27 -0400

    add more data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,6 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5ed8e2d06e69412de6940dbeeb7b7586&query=' + longitude + ',' + latitude;
-
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service', undefined);
@@ -11,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);
         } else {
             const current = body.current;
-            callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike)
+            callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees celcius out. It feels like " + current.feelslike + ' degrees with a precipitation of ' + current.precip + '.' +
+            ' With wind speeds of ' + current.wind_speed + ' mph. The uv index is at ' + current.uv_index + '.')
         }
     })
 }
